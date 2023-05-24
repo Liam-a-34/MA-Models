739304e91551bb585e2344652d5506bebe5aeb82
Liam Allen <liamallen343@gmail.com>
2023-05-24 17:57:37 -0500

    Finishing up the models page

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,3 +17,62 @@ leftButton.addEventListener("click", function(){
     portfolioCont.classList.add("move-left")
 
 })
+
+const parentElement = document.querySelector(".models-display-cont")
+const childCount = parentElement.children.length;
+
+document.getElementById("props-anchor").addEventListener("click", function() {
+    for(let i = 0; i < childCount; i++){
+        const childElement = parentElement.children[i]
+
+        if(childElement.classList.contains("props")){
+            childElement.style.display = "flex"
+        } else {
+            childElement.style.display = "none"
+        }
+    }
+})
+
+document.getElementById("weapons-anchor").addEventListener("click", function() {
+    for(let i = 0; i < childCount; i++){
+        const childElement = parentElement.children[i]
+
+        if(childElement.classList.contains("weapons")){
+            childElement.style.display = "flex"
+        } else {
+            childElement.style.display = "none"
+        }
+    }
+})
+
+document.getElementById("characters-anchor").addEventListener("click", function() {
+    for(let i = 0; i < childCount; i++){
+        const childElement = parentElement.children[i]
+
+        if(childElement.classList.contains("characters")){
+            childElement.style.display = "flex"
+        } else {
+            childElement.style.display = "none"
+        }
+    }
+})
+
+document.getElementById("environments-anchor").addEventListener("click", function() {
+    for(let i = 0; i < childCount; i++){
+        const childElement = parentElement.children[i]
+
+        if(childElement.classList.contains("environments")){
+            childElement.style.display = "flex"
+        } else {
+            childElement.style.display = "none"
+        }
+    }
+})
+
+document.getElementById("all-anchor").addEventListener("click", function() {
+    for(let i = 0; i < childCount; i++){
+        const childElement = parentElement.children[i]
+
+        childElement.style.display = "flex"
+    }
+})
